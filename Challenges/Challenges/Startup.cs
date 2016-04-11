2dc2e3ac4e8b855e9b5a55ddd3a30c8a1d4914d7
tkelly <tkelly@scottlogic.co.uk>
2016-04-11 15:11:23 +0100

    Remove unused packages

--- Challenges/Challenges/Startup.cs
+++ Challenges/Challenges/Startup.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.Owin;
-using Owin;
+﻿using Microsoft.Owin;
 
 [assembly: OwinStartup(typeof(Challenges.Startup))]
 
 namespace Challenges
 {
+    using Owin;
+
     public partial class Startup
     {
         public void Configuration(IAppBuilder app)
